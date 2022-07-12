bcd8b7cb7da560041e9595d1ed123f1dab35b077
prg0615 <parguzatt@gmail.com>
2022-07-12 22:11:00 +0200

    http request added

--- src/pages/NewQuotes.js
+++ src/pages/NewQuotes.js
@@ -1,16 +1,27 @@
+import { useEffect } from "react"
 import { useHistory } from "react-router-dom"
+import useHttp from "../hooks/use-http"
+import { addQuote } from "../lib/api"
 
 import QuoteForm from "../components/quotes/QuoteForm"
+
 const NewQuotes = () => {
+  const { sendRequest, status } = useHttp(addQuote)
   const history = useHistory()
 
-  const addQuoteHandler = (quoteData) => {
-    console.log(quoteData)
+  useEffect(() => {
+    if (status === "completed") {
+      history.push("/quotes")
+    }
+  }, [status, history])
 
-    history.push("/quotes")
+  const addQuoteHandler = (quoteData) => {
+    sendRequest(quoteData)
   }
 
-  return <QuoteForm onAddQuote={addQuoteHandler} />
+  return (
+    <QuoteForm isLoading={status === "panding"} onAddQuote={addQuoteHandler} />
+  )
 }
 
 export default NewQuotes
